66522bf61397faf9f9e801a31fa5d6bd5948f09c
xzer <xiaozhu@gmail.com>
2014-01-01 11:59:48 +0900

    for new guide of add-on package

--- build.bat
+++ build.bat
@@ -4,18 +4,7 @@ set v=%1
 mkdir build
 xcopy "%x%" build /i /e
 
-cd build\chrome
-7z a -tzip "%x%.jar" * -r -mx=0
-cd ..\..
-
-rmdir /s /q build\chrome\content
-rmdir /s /q build\chrome\locale
-rmdir /s /q build\chrome\skin
-
 cd build
-
-del chrome.manifest
-ren chrome.manifest.arch chrome.manifest
 
 7z a -tzip "%x%.xpi" * -r -mx=9
 cd ..
